471b6bd7804f6053e5d3b0e162afeef46d211181
oeduardodev <leite.eduardo@academico.ifpb.edu.br>
2023-07-24 20:11:22 -0300

    Estrutura visual - Tabela de transações.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,47 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 2.200,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 39,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>08/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 200,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
